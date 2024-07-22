cd6479a82f2fe9eb73e325f7e7a6c6cbd6e93c53
kimst <spvpt44@naver.com>
2024-07-23 00:03:35 +0900

    express 테스트

--- index.js
+++ index.js
@@ -1,1 +1,11 @@
-console.log("Hello World");
+const express = require('express');
+const app = express();
+const port = 3000;
+
+app.get('/', (req, res) => {
+    res.send('Hello');
+})
+
+app.listen(port, () => {
+    console.log('sample test');
+})
